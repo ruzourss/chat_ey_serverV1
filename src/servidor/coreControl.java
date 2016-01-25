dbc5af5f4ef096a84c8fbde3947cd55e5ddcfca6
ruzourss <tote.2.comarca@gmail.com>
2016-01-25 20:47:13 +0100

    se crea la clase core control , a la escucha del puerto de control

---
+++ src/servidor/coreControl.java
@@ -0,0 +1,21 @@
+package servidor;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 
+ * @author Tautvydas Bagocius
+ */
+public class coreControl extends Thread{ 
+    private int puerto;
+    private ServerSocket server;
+    private Socket canal;
+
+    @Override
+    public void run() {
+        
+    }
+    
+    
+}
