ad9012ab41ef0922e4c8a77568f188a213a0552a
ruzourss <tote.2.comarca@gmail.com>
2016-01-25 15:04:21 +0100

    se le añade comentarios para mejor visualización de código

--- src/servidor/core.java
+++ src/servidor/core.java
@@ -6,19 +6,26 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- *
+ * clase que inicaliza el funcionamiento del servidor, abre el puerto indicado
+ * y esta a la escucha de nuevos clientes, una vez que se conecta se crea un hilo y 
+ * se le pasa la conexión.
  * @author Tote
  */
 public class core {
-    
+    //declaramos todos los objetos y variables que vamos a necesitar
     private int puerto =8889;
     private mensajeria mensaje;
-    private ArrayList<cliente> hilos = new ArrayList<>();
-
+    private ArrayList<cliente> hilos;
+/**
+ * constructor por defecto que inicializa el objeto mensaje y la lista de clientes.
+ */
     public core() {
-    mensaje = new mensajeria();
+        mensaje = new mensajeria();
+        hilos =  new ArrayList<>();
     }
-    
+    /**
+     * método que arranca el servidor. Esta en un bucle infinito
+     */
     public void initServer(){
         try {
             ServerSocket s = new ServerSocket(puerto);
