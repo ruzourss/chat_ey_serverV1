5a8c45d7d37644468371ead4cbaebe2a27be1cc0
ruzourss <tote.2.comarca@gmail.com>
2016-01-25 15:04:34 +0100

    se le añade comentarios para mejor visualización de código

--- src/servidor/cliente.java
+++ src/servidor/cliente.java
@@ -1,4 +1,3 @@
-
 package servidor;
 
 import java.io.DataInputStream;
@@ -10,11 +9,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Hilo que simula el funcionamiento de un nuevo cliente
  * @author Tote
  */
 public class cliente extends Thread {
-    
+    //declaramos todos los objetos y variables que vamos a necesitar
     private Socket canal;
     private mensajeria mensaje;
     private String cadena;
@@ -22,10 +21,19 @@ public class cliente extends Thread {
     private DataOutputStream out;
     private DataInputStream input;
     
-
+    /**
+     * Constructor por defecto
+     */
     public cliente() {
     }
-
+    /**
+     * Constructor que recibe por parámetro
+     * @param canal cliente que se ha conectado
+     * @param mensaje objeto mensaje donde irán guardandose todos los mensajes
+     * @param hilos el array de hilos donde estarán todos los clientes , esto es
+     * necesario para realizar la silumación de un mensaje multicast, ya que cuando 
+     * un cliente escriba debe llegar a todos los usuarios conectados.
+     */
     public cliente(Socket canal, mensajeria mensaje,ArrayList<cliente> hilos ) {
         this.canal = canal;
         this.mensaje = mensaje;
@@ -38,7 +46,11 @@ public class cliente extends Thread {
         leer();
         cierraCanales();
     }
-    
+    /**
+     * Método leer que simplemente esta a la espera de recibir una cadena.
+     * Después la envia al objeto mensaje que ahi es donde se "contabiliza"
+     * el mensaje y se le renvia a todos los clientes conectados
+     */
     private void leer(){
         try {
             while((cadena=input.readUTF())!=null){
@@ -49,7 +61,11 @@ public class cliente extends Thread {
         } 
         
     }
-    
+    /**
+     * Este método debe ser público porque es llamado desde mensaje para renviar el mensaje 
+     * a los clientes.
+     * @param cadena cadena a enviar a los clientes
+     */
     public void escribir(String cadena){
         try {
             System.out.println("Enviar: "+cadena);
@@ -60,7 +76,9 @@ public class cliente extends Thread {
         }
         
     }
-    
+    /**
+     * Método que obtiene los canles por donde debe recibir y enviar los datos
+     */
     private void ObtieneCanales(){
         try {
             out = new DataOutputStream(canal.getOutputStream());
@@ -69,7 +87,9 @@ public class cliente extends Thread {
             Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
         }   
     }
-    
+    /**
+     * Método que cierra los canales cuando finaliza la conexión con el servidor
+     */
     private void cierraCanales(){
         if(input!=null){
             try {
