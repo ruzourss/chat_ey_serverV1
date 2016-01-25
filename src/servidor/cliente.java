09c040aa06285a53b1ac372aadf8215b496cdb87
ruzourss <tote.2.comarca@gmail.com>
2016-01-25 22:48:26 +0100

    se arregla el tema de las tildes y ñ

--- src/servidor/cliente.java
+++ src/servidor/cliente.java
@@ -1,8 +1,8 @@
 package servidor;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -20,8 +20,8 @@ public class cliente extends Thread {
     private mensajeria mensaje;
     private String cadenaRecibida;
     private ArrayList<cliente> hilos; 
-    private DataOutputStream out;
-    private DataInputStream input;
+    private ObjectOutputStream ObjectOutput;
+    private ObjectInputStream ObjectInput;
     private boolean session=true;
     private JTextField field;
     private JTextArea area;
@@ -38,6 +38,8 @@ public class cliente extends Thread {
      * @param hilos el array de hilos donde estarán todos los clientes , esto es
      * necesario para realizar la silumación de un mensaje multicast, ya que cuando 
      * un cliente escriba debe llegar a todos los usuarios conectados.
+     * @param field
+     * @param area
      */
     public cliente(Socket canal, mensajeria mensaje,ArrayList<cliente> hilos,JTextField field,JTextArea area ) {
         this.canal = canal;
@@ -60,49 +62,77 @@ public class cliente extends Thread {
     private void leer(){
         try {
             while(session){
-                cadenaRecibida=input.readUTF();
+                cadenaRecibida=ObjectInput.readUTF();
                 switch(cadenaRecibida){
                     case estados.SEND_NICK:
-                        out.writeUTF(estados.GET_NICK);
-                        out.flush();
+                        ObjectOutput.writeUTF(estados.GET_NICK);
+                        ObjectOutput.flush();
                         //a la espera de que reciba un nick
-                        String nick = input.readUTF();
+                        String nick = ObjectInput.readUTF();
                             if(compruebaNick(nick)){
-                                out.writeUTF(estados.GET_MESSAGES);
-                                out.flush();
+                                //en caso de que el nick sea el correcto le enviamos la confirmación
+                                ObjectOutput.writeUTF(estados.NICK_OK);
+                                ObjectOutput.flush();
                                 //modificamos el nombre del hilo si es el correcto
                                 setName(nick);
                                 field.setText("NÚMERO DE CONEXIONES ACTUALES: "+hilos.size());
                             }else{
-                                out.writeUTF(estados.NICK_ERROR);
-                                out.flush();
+                                ObjectOutput.writeUTF(estados.NICK_ERROR);
+                                ObjectOutput.flush();
                             }
                     break;
-                    case estados.EXIT:
-                        /*
-                        En caso de que el cliente le envia un EXIT, primero 
-                        recorremos el array en busca del hilo y lo eliminamos.
-                        */
-                        for(int i=0;i<hilos.size();i++){
-                            if(hilos.get(i).getName().equals(this.getName())){
-                                hilos.remove(i);
-                                break;
+                    case estados.GET_RECORD:
+                        //Aqui es donde le envia el historia de mensajes
+                        
+                    break;
+                    case estados.SEND_MESSAGES:
+                        //el cliente le avisa al servidor que este preparado para recibir 
+                        //mensajes del cliente, el servidor le avisa al cliente 
+                        //que esta preparado para recibir los mensajes
+                        ObjectOutput.writeUTF(estados.GET_MESSAGES);
+                        ObjectOutput.flush();
+                        String cadena;
+                        while(session){
+                            cadena = (String) ObjectInput.readObject();
+                            if(cadena.equals(estados.EXIT)){
+                                exit();
                             }
+                            System.out.println(cadena);
                         }
-                        // modificamos la variable session para que termine el bucle y su
-                        // ejecución
-                        session=false;
                     break;
                         default:
-                            mensaje.escribir(cadenaRecibida, hilos, this);
+                            //mensaje desconocido
+                            area.setText(area.getText()+" Mensaje desconocido, recibe: "+cadenaRecibida);
                     break;  
                 }
             }
             
-        } catch (IOException ex) {
+        } catch (IOException | ClassNotFoundException ex) {
             Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
         
+    }
+    
+    
+    
+    /**
+     * Método que elimina del array el hilo que quiere cerrar la conexión
+     * y pone la variable sesión a false para que se salga del bucle
+     */
+    private void exit() {
+        /*
+        En caso de que el cliente le envia un EXIT, primero
+        recorremos el array en busca del hilo y lo eliminamos.
+        */
+        for(int i=0;i<hilos.size();i++){
+            if(hilos.get(i).getName().equals(this.getName())){
+                hilos.remove(i);
+                break;
+            }
+        }
+        // modificamos la variable session para que termine el bucle y su
+        // ejecución
+        session=false;
     }
     /**
      * Este método debe ser público porque es llamado desde mensaje para renviar el mensaje 
@@ -112,8 +142,8 @@ public class cliente extends Thread {
     public void escribir(String cadena){
         try {
             System.out.println("Enviar: "+cadena);
-            out.writeUTF(cadena);
-            out.flush();
+            ObjectOutput.writeUTF(cadena);
+            ObjectOutput.flush();
         } catch (IOException ex) {
             Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -124,8 +154,8 @@ public class cliente extends Thread {
      */
     private void ObtieneCanales(){
         try {
-            out = new DataOutputStream(canal.getOutputStream());
-            input = new DataInputStream(canal.getInputStream());
+            ObjectOutput = new ObjectOutputStream(canal.getOutputStream());
+            ObjectInput = new ObjectInputStream(canal.getInputStream());
         } catch (IOException ex) {
             Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
         }   
@@ -134,15 +164,15 @@ public class cliente extends Thread {
      * Método que cierra los canales cuando finaliza la conexión con el servidor
      */
     private void cierraCanales(){
-        if(input!=null){
+        if(ObjectInput!=null){
             try {
-                input.close();
+                ObjectInput.close();
             } catch (IOException ex) {
                 Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
             }
-        }if(out!=null){
+        }if(ObjectOutput!=null){
             try {
-                out.close();
+                ObjectOutput.close();
             } catch (IOException ex) {
                 Logger.getLogger(cliente.class.getName()).log(Level.SEVERE, null, ex);
             }
