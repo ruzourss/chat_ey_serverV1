bcdac5a58b3984d732bf861db09a23cd19378462
ruzourss <tote.2.comarca@gmail.com>
2016-02-03 02:25:52 +0100

    Se crea el puerto de control que es el encargado de enviar la lista de los clientes conectados

--- src/servidor/coreControl.java
+++ src/servidor/coreControl.java
@@ -1,7 +1,13 @@
 package servidor;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextArea;
 
 /**
  * 
@@ -11,11 +17,44 @@ public class coreControl extends Thread{
     private int puerto;
     private ServerSocket server;
     private Socket canal;
+    private ArrayList<Socket> contenedorCanales;
+    private ArrayList<String> clientes;
+    private JTextArea area;
 
-    @Override
-    public void run() {
-        
+    public coreControl(int puerto,ArrayList<String> clientes,JTextArea area) {
+        this.puerto=puerto;
+        contenedorCanales = new ArrayList<>();
+        this.clientes=clientes;
+        this.area=area;
     }
     
+    @Override
+    public void run() {
+       initServer();
+    }
+    
+    private void initServer(){
+        try {
+            server = new ServerSocket(puerto);
+            area.append("Iniciado servicio de control puerto: "+puerto+"\n");
+            while(true){
+                canal = server.accept();
+                contenedorCanales.add(canal);
+                new ObjectOutputStream(canal.getOutputStream()).writeObject(clientes);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(coreControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void envioListadoUsuarios(){ 
+        for(int i=0;i<contenedorCanales.size();i++){
+           try {
+                new ObjectOutputStream(contenedorCanales.get(i).getOutputStream()).writeObject(clientes);
+            } catch (IOException ex) {
+                Logger.getLogger(coreControl.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }  
+    }
     
 }
