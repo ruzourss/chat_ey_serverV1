300e2b0f9c07780455c6f7cedae50dac9c2002f5
ruzourss <tote.2.comarca@gmail.com>
2016-02-03 01:23:59 +0100

    se añade el sistema del guardado del historial y tamaño del buffer

--- src/servidor/mensajeria.java
+++ src/servidor/mensajeria.java
@@ -1,5 +1,6 @@
 package servidor;
 import java.util.ArrayList;
+import javax.swing.JTextArea;
 /**
  *
  * @author Tote
@@ -8,14 +9,19 @@ public class mensajeria {
     
     //private String mensajes;
     private ArrayList<String> mensajes;
+    private JTextArea area;
     
-    
-    public mensajeria() {
+    public mensajeria(JTextArea area) {
         mensajes = new ArrayList<>();
+        this.area=area;
     }
     
-    public void escribir(String cadena,ArrayList<cliente> hilos){
-        mensajes.add(cadena);
+    public void escribir(String cadena,ArrayList<cliente> hilos, Thread hilo){
+        area.append(hilo.getName()+">"+cadena);
+        if(getMensajes().size()>25){
+            getMensajes().remove(0);
+        }
+        getMensajes().add(cadena);
         System.out.println("Recibido: "+cadena);
         //le enviamos el mensaje a los clientes que estan dentro del array,
         //realmnete estamos haciendo aqui el multicast
@@ -23,4 +29,12 @@ public class mensajeria {
             hilos.get(i).escribir(cadena);
         }
     }
+
+    /**
+     * @return the mensajes
+     */
+    public ArrayList<String> getMensajes() {
+        return mensajes;
+    }
+    
 }
